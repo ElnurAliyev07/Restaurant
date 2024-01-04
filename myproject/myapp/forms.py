0fb62143b395dd8893c40c3fcfc91faccaefa7b9
ElnurAliyev07 <115114253+ElnurAliyev07@users.noreply.github.com>
2024-01-04 17:51:14 +0400

    Delete myproject directory

--- myproject/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Reservation
-
-class ReservationForm(forms.ModelForm):
-    class Meta:
-        model = Reservation
-        fields = ['date', 'table', 'order']
